a6495d158e06c62666f7658e0caea80ba337b2fa
quangnvo <voquang.usth@gmail.com>
2023-12-25 10:58:04 +0800

    add the Demo section

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -3,13 +3,24 @@ import { copy, linkIcon, loader, tick } from "../assets"
 
 
 const Demo = () => {
+
+	const [article, setArticle] = useState({
+		url: "",
+		summary: "",
+	})
+
+	const handleSubmit = async (e) => {
+		alert("handle submit")
+	}
+
+
 	return (
 		<section className='mt-16 w-full max-w-xl'>
 			{/* Search */}
 			<div className='flex flex-col w-full gap-2'>
 				<form
 					className='relative flex justify-center items-center'
-					onSubmit={() => { }}
+					onSubmit={handleSubmit}
 				>
 					<img
 						src={linkIcon}
@@ -20,8 +31,8 @@ const Demo = () => {
 					<input
 						type='url'
 						placeholder='Paste the article link'
-						value=""
-						onChange={() => { }}
+						value={article.url}
+						onChange={(e) => setArticle({ ...article, url: e.target.value })}
 						onKeyDown={() => { }}
 						required
 						className='url_input peer' // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
@@ -33,28 +44,6 @@ const Demo = () => {
 						<p>↵</p>
 					</button>
 				</form>
-
-				{/* Browse History */}
-				{/* <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
-					{allArticles.reverse().map((item, index) => (
-						<div
-							key={`link-${index}`}
-							onClick={() => setArticle(item)}
-							className='link_card'
-						>
-							<div className='copy_btn' onClick={() => handleCopy(item.url)}>
-								<img
-									src={copied === item.url ? tick : copy}
-									alt={copied === item.url ? "tick_icon" : "copy_icon"}
-									className='w-[40%] h-[40%] object-contain'
-								/>
-							</div>
-							<p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
-								{item.url}
-							</p>
-						</div>
-					))}
-				</div> */}
 			</div>
 		</ section>
 	)
