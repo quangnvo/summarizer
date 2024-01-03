544de18c1ae82ce7a1502e6b5b29f0338cf4e2f0
quangnvo <voquang.usth@gmail.com>
2024-01-03 08:21:51 +0700

    add options to article slice

--- src/services/article.js
+++ src/services/article.js
@@ -2,6 +2,19 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
+const options = {
+    method: 'GET',
+    url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
+    params: {
+        url: 'https://time.com/6266679/musk-ai-open-letter/',
+        length: '3'
+    },
+    headers: {
+        'X-RapidAPI-Key': 'ae47ccf294mshb5260fb8eafb77bp1b4cedjsnd2d854fcd997',
+        'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
+    }
+};
+
 export const articleApi = createApi({
     reducerPath: 'articleApi',
     baseQuery: fetchBaseQuery({
